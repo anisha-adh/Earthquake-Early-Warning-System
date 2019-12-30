61173cc3e624ea4333d0e5744044f147fa963af8
GairePravesh <praveshgaire.ioe@gmail.com>
2019-12-30 14:53:18 +0545

    updated visualizer

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 from matplotlib import pyplot as plt
 import sqlite3
 conn = sqlite3.connect("readings_database.db")
@@ -17,11 +18,20 @@ cur.execute("""CREATE TABLE IF NOT EXISTS readings_table(
 cur.close()
 conn.close()
 '''
-data = cur.execute("select x,y,z from readings_table where id<100")
+values = int(sys.argv[1])
+command = sys.argv[2]
+data = cur.execute(command)
+#data = cur.execute("select x,y,z from readings_table where id<100")
 new_data = np.array(data.fetchall())
 cur.close()
 conn.close()
-plt.plot(new_data[:,0])
-plt.plot(new_data[:,1])
-#plt.plot(new_data[:,2])
+idx = 321
+title = ['X value', 'Y value', 'Z value', 'ax value', 'ay value', 'az value']
+fig = plt.figure()
+fig.tight_layout()
+for i in range(values):
+    ax = fig.add_subplot(idx+i);
+    ax.title.set_text(title[i])
+    ax.plot(new_data[:,i])
+plt.subplots_adjust(left=0.1, bottom=0.1, right=0.9, top=0.9, wspace=0.2, hspace=0.5)
 plt.show()
